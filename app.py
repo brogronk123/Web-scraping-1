fa54021fc044ce6c0e93c64478fa41e22f3383b6
brogronk123 <71152842+brogronk123@users.noreply.github.com>
2021-06-20 22:46:45 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,24 +3,21 @@ import requests
 import pandas as pd
 
 
-url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
+bright_stars_url = 'https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars'
 
-page = requests.get(url)
+page = requests.get(bright_stars_url)
 print(page)
 
 soup = bs(page.text,'html.parser')
 
-star_table = soup.find_all('table')
-print(len(star_table))
-
+star_table = soup.find('table')
 
 temp_list= []
-table_rows = star_table[4].find_all('tr')
+table_rows = star_table.find_all('tr')
 for tr in table_rows:
     td = tr.find_all('td')
     row = [i.text.rstrip() for i in td]
     temp_list.append(row)
-print(temp_list)
 
 
 
@@ -28,16 +25,16 @@ Star_names = []
 Distance =[]
 Mass = []
 Radius =[]
-
+Lum = []
 
 for i in range(1,len(temp_list)):
+    Star_names.append(temp_list[i][1])
+    Distance.append(temp_list[i][3])
+    Mass.append(temp_list[i][5])
+    Radius.append(temp_list[i][6])
+    Lum.append(temp_list[i][7])
     
-    Star_names.append(temp_list[i][0])
-    Distance.append(temp_list[i][5])
-    Mass.append(temp_list[i][7])
-    Radius.append(temp_list[i][8])
-
-df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,Lum)),columns=['Star_name','Distance','Mass','Radius','Luminosity'])
 print(df2)
 
-df2.to_csv('dwarf_stars.csv')
+df2.to_csv('bright_stars.csv')
